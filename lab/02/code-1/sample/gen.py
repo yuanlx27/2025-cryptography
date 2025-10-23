897d26dce0382357baa831f66696c5db7cf1fedd
John Smith <johnsmith0x3f@gmail.com>
2025-10-23 16:44:02 +0000

    chore: work on lab 02

--- lab/02/code-1/sample/gen.py
+++ lab/02/code-1/sample/gen.py
@@ -1,14 +1,19 @@
 input_hex = """
-81
-00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F
-00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F
-20 00 00 00
-76 D0 62 7D A1 D2 90 43 6E 21 A4 AF 7F CA 94 B7
-17 7C 1F C9 41 73 D4 42 E3 6E E7 9D 7C A0 E4 61
+01 00 00 00
+
+00
+05 20 00 00 00 00 00 00
+00 00 00 00 00 00 00 00
+00 00 00 00 00 00 00 00
+21 00 00 00 00 00 00 00
+00 00 00 00 00 00 00 00
+04 00 00 00 00 00 00 00
 """
 
 answer_hex = """
-00 11 22 33 44 55 66 77 88 99 AA BB CC DD EE FF
+24 20 00 00 00 00 00 00
+00 00 00 00 00 00 00 00
+04 00 00 00 00 00 00 00
 """
 
 
@@ -17,5 +22,5 @@ def dump(hex_str, filename):
         file.write(bytes.fromhex("".join(hex_str.split())))
 
 
-dump(input_hex, "samples/input.bin")
-dump(answer_hex, "samples/answer.bin")
+dump(input_hex, "sample/input.bin")
+dump(answer_hex, "sample/answer.bin")
--- lab/02/code-1/src/main.rs
+++ lab/02/code-1/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::io;
-use finite_field::u131;
+use std::ops::{Add, Mul};
 
 fn main() -> io::Result<()> {
     let mut reader: Box<dyn io::Read> = if cfg!(feature = "online_judge") {
@@ -15,170 +15,93 @@ fn main() -> io::Result<()> {
     };
 
     let n = {
-        let mut buffer = [0u8; 4];
-        reader.read_exact(&mut buffer).unwrap();
-        u32::from_le_bytes(buffer) as usize
+        let mut buf = [0u8; 4];
+        reader.read_exact(&mut buf)?;
+        u32::from_le_bytes(buf) as usize
     };
 
     for _ in 0..n {
         let op_type = {
-            let mut buffer = [0u8; 1];
-            reader.read_exact(&mut buffer).unwrap();
-            buffer[0] as usize
+            let mut buf = [0u8; 1];
+            reader.read_exact(&mut buf)?;
+            buf[0]
         };
 
         let a = {
-            let mut buffer = [0u8; 24];
-            reader.read_exact(&mut buffer).unwrap();
-            u131::from_le_bytes(buffer)
+            let mut buf = [0u8; 24];
+            reader.read_exact(&mut buf)?;
+            u131::from_le_bytes(buf)
         };
 
         let b = {
-            let mut buffer = [0u8; 24];
-            reader.read_exact(&mut buffer).unwrap();
-            u131::from_le_bytes(buffer)
+            let mut buf = [0u8; 24];
+            reader.read_exact(&mut buf)?;
+            u131::from_le_bytes(buf)
         };
 
         let result = match op_type {
             0 => a + b,
             1 => a * b,
+            2 => a.sqr(),
+            3 => a.inv(),
             _ => panic!("Invalid operation type."),
         };
-        writer.write_all(&result.into_le_bytes()).unwrap();
+        writer.write_all(&result.into_le_bytes())?;
     }
 
     Ok(())
 }
 
-mod finite_field {
-    use core::cmp::{Ordering, PartialEq, PartialOrd};
-    use core::ops::{Add, Mul, Shl};
+#[allow(non_camel_case_types)]
+struct u131 {
+    data: [u128; 3],
+}
 
-    #[allow(non_camel_case_types)]
-    #[derive(Clone, Copy, Default, PartialEq)]
-    pub struct u131 {
-        data: [u128; 3],
-    }
+impl Add for u131 {
+    type Output = Self;
 
-    const N: usize = 131;
-
-    const P: u131 = u131 {
-        data: [
-            0x00000000_00000000_00000000_00002007,
-            0x00000000_00000000_00000000_00000008,
-            0x00000000_00000000_00000000_00000000,
-        ],
-    };
-
-    impl Add for u131 {
-        type Output = Self;
-
-        fn add(self, rhs: Self) -> Self::Output {
-            Self {
-                data: [
-                    self.data[0] ^ rhs.data[0],
-                    self.data[1] ^ rhs.data[1],
-                    self.data[2] ^ rhs.data[2],
-                ],
-            }
+    fn add(self, other: Self) -> Self {
+        Self {
+            data: [
+                self.data[0] ^ other.data[0],
+                self.data[1] ^ other.data[1],
+                0,
+            ],
         }
     }
+}
 
-    impl Mul for u131 {
-        type Output = Self;
+impl Mul for u131 {
+    type Output = Self;
 
-        // fn mul(self, rhs: Self) -> Self::Output {
-        //     let mut result = Self::default();
-        //     for i in 0..N {
-        //         for j in 0..N {
-        //             let x = (i / 128, i % 128);
-        //             let y = (j / 128, j % 128);
-        //             let z = ((i + j) / 128, (i + j) % 128);
-        //             result.data[z.0] ^= ((self.data[x.0] >> x.1) & 1) & ((rhs.data[y.0] >> y.1) & 1) << z.1;
-        //         }
-        //     }
-        //     result.rem()
-        // }
+    fn mul(self, other: Self) -> Self {
+        todo!()
+    }
+}
 
-        fn mul(self, rhs: Self) -> Self::Output {
-            let mut result = Self::default();
-            for i in 0..N {
-                let x = (i / 128, i % 128);
-                if (self.data[x.0] >> x.1) & 1 != 0 {
-                    result = result + (rhs << i);
-                }
-            }
-            result.rem()
+impl u131 {
+    fn from_le_bytes(bytes: [u8; 24]) -> Self {
+        Self {
+            data: [
+                u128::from_le_bytes(bytes[0..16].try_into().unwrap()),
+                u64::from_le_bytes(bytes[16..24].try_into().unwrap()) as u128,
+                0,
+            ],
         }
     }
 
-    impl PartialOrd for u131 {
-        fn partial_cmp(&self, rhs: &Self) -> Option<Ordering> {
-            for i in (0..3).rev() {
-                if self.data[i] < rhs.data[i] {
-                    return Some(Ordering::Less);
-                } else if self.data[i] > rhs.data[i] {
-                    return Some(Ordering::Greater);
-                }
-            }
-            Some(Ordering::Equal)
-        }
+    fn into_le_bytes(self) -> [u8; 24] {
+        let mut bytes = [0u8; 24];
+        bytes[0..16].copy_from_slice(&self.data[0].to_le_bytes());
+        bytes[16..24].copy_from_slice(&(self.data[1] as u64).to_le_bytes());
+        bytes
     }
 
-    impl Shl<usize> for u131 {
-        type Output = Self;
-
-        fn shl(self, rhs: usize) -> Self::Output {
-            let mut result = Self::default();
-            for i in 0..3 {
-                let x = (rhs / 128, (rhs % 128) as u32);
-                if i >= x.0 {
-                    result.data[i] |= self.data[i - x.0].wrapping_shl(x.1);
-                    if i > x.0 {
-                        result.data[i] |= self.data[i - x.0 - 1].wrapping_shr(128 - x.1);
-                    }
-                }
-            }
-            result
-        }
+    fn sqr(&self) -> Self {
+        todo!()
     }
 
-    impl u131 {
-        pub fn from_le_bytes(bytes: [u8; 24]) -> Self {
-            Self {
-                data: [
-                    u128::from_le_bytes(bytes[0..16].try_into().unwrap()),
-                    u64::from_le_bytes(bytes[16..24].try_into().unwrap()) as u128,
-                    0,
-                ],
-            }
-        }
-
-        pub fn into_le_bytes(self) -> [u8; 24] {
-            let mut bytes = [0u8; 24];
-            bytes[0..16].copy_from_slice(&self.data[0].to_le_bytes());
-            bytes[16..24].copy_from_slice(&(self.data[1] as u64).to_le_bytes());
-            bytes
-        }
-
-        // fn rem(self) -> Self {
-        //     let mut result = self;
-        //     result.data[0] ^= result.data[1].wrapping_shr(3) ^ result.data[1].wrapping_shr(2) ^ result.data[1].wrapping_shr(1) ^ result.data[1].wrapping_shl(10);
-        //     result.data[0] ^= result.data[2].wrapping_shl(125) ^ result.data[2].wrapping_shl(126) ^ result.data[2].wrapping_shl(127);
-        //     result.data[1] ^= result.data[2].wrapping_shr(3) ^ result.data[2].wrapping_shr(2) ^ result.data[2].wrapping_shr(1) ^ result.data[2].wrapping_shl(10);
-        //     result.data[1] &= 0x7;
-        //     result.data[2] &= 0;
-        //     result
-        // }
-
-        fn rem(self) -> Self {
-            let mut result = self;
-            for i in (0..N).rev() {
-                if result >= (P << i) {
-                    result = result + (P << i);
-                }
-            }
-            result
-        }
+    fn inv(&self) -> Self {
+        todo!()
     }
 }
