e5cc7704fe2a359962bfb43e2adda715df3ce693
John Smith <johnsmith0x3f@gmail.com>
2025-10-29 04:11:41 +0000

    chore: finish lab 02

--- lab/02/code-1/src/main.rs
+++ lab/02/code-1/src/main.rs
@@ -1,5 +1,7 @@
 use std::fs;
 use std::io;
+use std::mem::swap;
+use std::cmp::{PartialOrd, Ordering};
 use std::ops::{Add, Mul, Shl};
 
 const N: usize = 131;
@@ -55,9 +57,35 @@ fn main() -> io::Result<()> {
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Clone, Copy, Default)]
+#[derive(Clone, Copy, Default, PartialEq)]
 struct u131 {
-    data: [u128; 3],
+    data: [u64; 5],
+}
+
+const P: u131 = u131 {
+    data: [
+        0x00000000_00002007,
+        0,
+        0x00000000_00000008,
+        0,
+        0,
+    ],
+};
+
+const ZERO: u131 = u131 { data: [0; 5] };
+const ONE: u131 = u131 { data: [1, 0, 0, 0, 0] };
+
+impl PartialOrd for u131 {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        for i in (0..5).rev() {
+            if self.data[i] < other.data[i] {
+                return Some(Ordering::Less);
+            } else if self.data[i] > other.data[i] {
+                return Some(Ordering::Greater);
+            }
+        }
+        Some(Ordering::Equal)
+    }
 }
 
 impl Add for u131 {
@@ -69,6 +97,8 @@ impl Add for u131 {
                 self.data[0] ^ rhs.data[0],
                 self.data[1] ^ rhs.data[1],
                 self.data[2] ^ rhs.data[2],
+                self.data[3] ^ rhs.data[3],
+                self.data[4] ^ rhs.data[4],
             ],
         }
     }
@@ -80,7 +110,7 @@ impl Mul for u131 {
     fn mul(self, rhs: Self) -> Self {
         let mut res = Self::default();
         for i in 0..N {
-            if (self.data[i / 128] >> (i % 128)) & 1 != 0 {
+            if (self.data[i / 64] >> (i % 64)) & 1 != 0 {
                 res = res + (rhs << i);
             }
         }
@@ -88,35 +118,45 @@ impl Mul for u131 {
     }
 }
 
+// impl Div for u131 {
+//     type Output = Self;
+
+//     fn div(self, rhs: Self) -> Self {
+//         let mut rem = self;
+//         let mut res = Self::default();
+//         for i in (0..N).rev() {
+//             if rem.len() == rhs.len() + i {
+//                 rem = rem + (rhs << i);
+//                 res.data[i / 64] |= 1 << (i % 64);
+//             }
+//         }
+//         res
+//     }
+// }
+
 impl Shl<usize> for u131 {
     type Output = Self;
 
     fn shl(self, rhs: usize) -> Self {
-        match rhs {
-            0 => self,
-            1..128 => Self {
-                data: [
-                    (self.data[0] << rhs),
-                    (self.data[1] << rhs) ^ (self.data[0] >> (128 - rhs)),
-                    (self.data[2] << rhs) ^ (self.data[1] >> (128 - rhs)),
-                ],
-            },
-            128 => Self {
-                data: [
-                    0,
-                    self.data[0],
-                    self.data[1],
-                ],
-            },
-            129..N => Self {
-                data: [
-                    0,
-                    (self.data[0] << (rhs - 128)),
-                    (self.data[1] << (rhs - 128)) ^ (self.data[0] >> (256 - rhs)),
-                ],
-            },
-            _ => panic!("Shift amount out of range."),
+        let mut res = Self::default();
+        let (x, y) = (rhs / 64, rhs % 64);
+
+        for i in (x..5).rev() {
+            res.data[i] = self.data[i - x];
         }
+
+        if y == 0 {
+            return res;
+        }
+
+        for i in (x..5).rev() {
+            res.data[i] <<= y;
+            if i > 0 {
+                res.data[i] ^= res.data[i - 1] >> (64 - y);
+            }
+        }
+
+        res
     }
 }
 
@@ -124,8 +164,10 @@ impl u131 {
     fn from_le_bytes(bytes: [u8; 24]) -> Self {
         Self {
             data: [
-                u128::from_le_bytes(bytes[0..16].try_into().unwrap()),
-                u64::from_le_bytes(bytes[16..24].try_into().unwrap()) as u128,
+                u64::from_le_bytes(bytes[0..8].try_into().unwrap()),
+                u64::from_le_bytes(bytes[8..16].try_into().unwrap()),
+                u64::from_le_bytes(bytes[16..24].try_into().unwrap()),
+                0,
                 0,
             ],
         }
@@ -133,43 +175,80 @@ impl u131 {
 
     fn into_le_bytes(self) -> [u8; 24] {
         let mut bytes = [0u8; 24];
-        bytes[0..16].copy_from_slice(&self.data[0].to_le_bytes());
-        bytes[16..24].copy_from_slice(&(self.data[1] as u64).to_le_bytes());
+        bytes[0..8].copy_from_slice(&self.data[0].to_le_bytes());
+        bytes[8..16].copy_from_slice(&self.data[1].to_le_bytes());
+        bytes[16..24].copy_from_slice(&self.data[2].to_le_bytes());
         bytes
+    }
+
+    fn deg(&self) -> usize {
+        for i in (0..5).rev() {
+            if self.data[i] != 0 {
+                return i * 64 + self.data[i].ilog2() as usize;
+            }
+        }
+        panic!("Degree of zero polynomial is undefined.");
     }
 
     fn rem(&self) -> Self {
         let mut res = *self;
 
-        let temp = res.data[2] >> 118;
-        res.data[1] ^= temp << 115 ^ temp << 116 ^ temp << 117;
-        res.data[2] ^= temp ^ temp << 118;
+        for i in (3..5).rev() {
+            res.data[i - 3] ^= res.data[i] << 61 ^ res.data[i] << 62 ^ res.data[i] << 63;
+            res.data[i - 2] ^= res.data[i] >> 3 ^ res.data[i] >> 2 ^ res.data[i] >> 1 ^ res.data[i] << 10;
+            res.data[i - 1] ^= res.data[i] >> 54;
+        }
+
+        res.data[4] = 0;
+        res.data[3] = 0;
 
         let temp = res.data[2] >> 3;
-        res.data[1] ^= temp ^ temp << 1 ^ temp << 2 ^ temp << 13;
-        res.data[2] ^= temp << 3;
-
-        let temp = res.data[2];
-        res.data[0] ^= temp << 125 ^ temp << 126 ^ temp << 127;
-        res.data[1] ^= temp >> 2 ^ temp >> 1 ^ temp << 10;
-        res.data[2] ^= temp;
-
-        let temp = res.data[1] >> 118;
-        res.data[0] ^= temp << 115 ^ temp << 116 ^ temp << 117;
-        res.data[1] ^= temp ^ temp << 118;
-
-        let temp = res.data[1] >> 3;
         res.data[0] ^= temp ^ temp << 1 ^ temp << 2 ^ temp << 13;
-        res.data[1] ^= temp << 3;
+        res.data[1] ^= temp >> 51;
+        res.data[2] &= 0x7;
 
         res
     }
 
     fn sqr(&self) -> Self {
-        todo!()
+        let mut res = Self::default();
+        for i in 0..N {
+            let j = i * 2;
+            res.data[j / 64] |= ((self.data[i / 64] >> (i % 64)) & 1) << (j % 64);
+        }
+        res.rem()
     }
 
     fn inv(&self) -> Self {
-        todo!()
+        self.inv_gcd()
+    }
+
+    #[allow(unused)]
+    fn inv_gcd(&self) -> Self {
+        let (mut u, mut v) = (*self, P);
+        let (mut g1, mut g2) = (ONE, ZERO);
+        while u != ONE {
+            let j = u.deg().checked_sub(v.deg());
+            if j == None {
+                swap(&mut u, &mut v);
+                swap(&mut g1, &mut g2);
+                continue;
+            }
+            let j = j.unwrap();
+            u = (u + (v << j)).rem();
+            g1 = (g1 + (g2 << j)).rem();
+        }
+        g1
+    }
+
+    #[allow(unused)]
+    fn inv_pow(&self) -> Self {
+        let mut a = *self;
+        let mut res = Self { data: [1, 0, 0, 0, 0] };
+        for _ in 0..(N - 1) {
+            res = (res * a).rem();
+            a = a.sqr().rem();
+        }
+        res.sqr().rem()
     }
 }
